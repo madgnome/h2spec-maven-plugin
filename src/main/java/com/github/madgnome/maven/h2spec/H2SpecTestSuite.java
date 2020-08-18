289979d9c991aa4125d390bf073dfed97b1281b1
Olivier Lamy <oliver.lamy@gmail.com>
2020-08-18 19:39:25 +1000

    Use Docker to run h2spec tests (#9)
    * use docker to run spec
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * get this working only with docker
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * fix it test
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * dependabot only weekly
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * remove copy/paste
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * build only on linux
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * ncleanup imports
    
    Signed-off-by: olivier lamy <olamy@apache.org>
    
    * cleanup dependencies not used
    
    Signed-off-by: olivier lamy <olamy@apache.org>

--- src/main/java/com/github/madgnome/maven/h2spec/H2SpecTestSuite.java
+++ src/main/java/com/github/madgnome/maven/h2spec/H2SpecTestSuite.java
@@ -1,27 +1,12 @@
 package com.github.madgnome.maven.h2spec;
 
-import org.apache.commons.exec.CommandLine;
-import org.apache.commons.exec.DefaultExecutor;
-import org.apache.commons.exec.Executor;
-import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.maven.plugin.logging.Log;
-import org.apache.maven.plugin.logging.SystemStreamLog;
 import org.apache.maven.plugins.surefire.report.ReportTestCase;
 import org.apache.maven.plugins.surefire.report.ReportTestSuite;
 import org.apache.maven.plugins.surefire.report.SurefireReportParser;
 import org.apache.maven.reporting.MavenReportException;
-import org.codehaus.plexus.archiver.UnArchiver;
-import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
-import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
-import org.codehaus.plexus.logging.AbstractLogEnabled;
-import org.codehaus.plexus.logging.Logger;
-import org.codehaus.plexus.logging.console.ConsoleLogger;
-import org.codehaus.plexus.util.FileUtils;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -33,58 +18,9 @@ public class H2SpecTestSuite
 
     public static final String DEFAULT_VERSION = "2.6.0";
 
-    public static void main(String[] args) throws IOException
-    {
-        Config config = new Config();
-        config.log = new SystemStreamLog();
-        config.outputDirectory = new File(args[0]);
-        config.port = Integer.parseInt(args[1]);
-        config.timeout = 2;
-        config.excludeSpecs = Collections.emptySet();
-
-        runH2Spec(config);
-    }
-
     public static String getSpecIdentifier(String specId, String name)
     {
         return specId + " - " + name;
-    }
-
-    public static File buildJunitFile(Config config){
-        File reportsDirectory = new File(config.outputDirectory, "surefire-reports");
-        if (!Files.exists(reportsDirectory.toPath()))
-        {
-            config.log.debug("Reports directory " + reportsDirectory.getAbsolutePath() + " does not exist, try creating it...");
-            if (reportsDirectory.mkdirs())
-            {
-                config.log.debug("Reports directory " + reportsDirectory.getAbsolutePath() + " created.");
-            }
-            else
-            {
-                config.log.debug("Failed to create report directory");
-            }
-        }
-        return new File(reportsDirectory, config.junitFileName);
-    }
-
-    public static List<Failure> runH2Spec(Config config) throws IOException
-    {
-        File junitFile = buildJunitFile(config);
-        File h2spec = getH2SpecFile(config.outputDirectory, config.version);
-
-        Executor exec = new DefaultExecutor();
-        PumpStreamHandler psh = new PumpStreamHandler(System.out, System.err, System.in);
-        exec.setStreamHandler(psh);
-        exec.setExitValues(new int[]{0, 1});
-
-        psh.start();
-        if (exec.execute(buildCommandLine(h2spec, junitFile, config)) != 0)
-        {
-            return parseReports(config.log, junitFile.getParentFile(), config.excludeSpecs);
-        }
-        psh.stop();
-
-        return Collections.emptyList();
     }
 
     public static List<Failure> parseReports(final Log logger, final File reportsDirectory, final Set<String> excludeSpecs)
@@ -129,91 +65,9 @@ public class H2SpecTestSuite
         }
         catch (MavenReportException e)
         {
-            e.printStackTrace();
+            logger.warn( e.getMessage(), e );
         }
 
         return failures;
     }
-
-    private static CommandLine buildCommandLine(File h2spec, File junitFile, Config config)
-    {
-        String command = String.format("%s %s -p %d -j %s -o %d --max-header-length %d",
-                h2spec.getAbsolutePath(), " ", config.port, junitFile.getAbsolutePath(),
-                                             config.timeout, config.maxHeaderLength);
-        config.log.info("h2spec command: " + command);
-        if (config.verbose)
-        {
-            command += " -v";
-        }
-        return CommandLine.parse(command);
-    }
-
-    private static File getH2SpecFile(final File targetDirectory, String version) throws IOException
-    {
-        URL h2SpecArchiveInJar = H2SpecTestSuite.class.getResource(getH2SpecArchivePathForOs(version));
-
-        File h2SpecArchive = new File(targetDirectory, new File(h2SpecArchiveInJar.getPath()).getName());
-        FileUtils.copyURLToFile(h2SpecArchiveInJar, h2SpecArchive);
-
-        File h2Spec = new File(targetDirectory, "h2spec");
-        UnArchiver unArchiver;
-        if (h2SpecArchive.getName().endsWith(".tar.gz"))
-        {
-            unArchiver = new TarGZipUnArchiver();
-        }
-        else
-        {
-            unArchiver = new ZipUnArchiver();
-        }
-        ((AbstractLogEnabled) unArchiver).enableLogging(new ConsoleLogger(Logger.LEVEL_DEBUG, "console"));
-
-        unArchiver.setSourceFile(h2SpecArchive);
-        unArchiver.setDestDirectory(targetDirectory);
-        unArchiver.extract();
-
-        if (!h2Spec.setExecutable(true))
-        {
-            throw new RuntimeException("Can't set h2spec as executable");
-        }
-        return h2Spec;
-    }
-
-    private static String getH2SpecArchivePathForOs(String version)
-    {
-        String os = System.getProperty("os.name").toLowerCase();
-        String fileName;
-        if (os.contains("win"))
-        {
-            fileName = "h2spec_windows_amd64.zip";
-        }
-        else if (os.contains("nix") || os.contains("nux") || os.contains("aix"))
-        {
-            fileName = "h2spec_linux_amd64.tar.gz";
-        }
-        else if (os.contains("mac"))
-        {
-            fileName = "h2spec_darwin_amd64.tar.gz";
-        }
-        else
-        {
-            throw new IllegalStateException("This OS is not supported.");
-        }
-
-        return String.format("/h2spec/%s/%s", version, fileName);
-    }
-
-
-    static class Config
-    {
-        Log log;
-        File outputDirectory;
-        int port;
-        int timeout;
-        int maxHeaderLength;
-        Set excludeSpecs;
-        String junitFileName = "TEST-h2spec.xml";
-        boolean verbose;
-        String version;
-    }
-
 }
--- src/main/java/com/github/madgnome/maven/h2spec/Http2SpecMojo.java
+++ src/main/java/com/github/madgnome/maven/h2spec/Http2SpecMojo.java
@@ -16,19 +16,26 @@ package com.github.madgnome.maven.h2spec;
  * limitations under the License.
  */
 
+import com.github.dockerjava.api.command.LogContainerCmd;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
 import org.slf4j.LoggerFactory;
+import org.testcontainers.DockerClientFactory;
+import org.testcontainers.Testcontainers;
+import org.testcontainers.containers.BindMode;
+import org.testcontainers.containers.ContainerLaunchException;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.FrameConsumerResultCallback;
+import org.testcontainers.containers.output.OutputFrame;
 import org.testcontainers.containers.output.Slf4jLogConsumer;
-import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.testcontainers.containers.output.WaitingConsumer;
+import org.testcontainers.containers.wait.strategy.AbstractWaitStrategy;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.File;
@@ -48,9 +55,14 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
 
 import static com.github.madgnome.maven.h2spec.H2SpecTestSuite.DEFAULT_VERSION;
+import static org.testcontainers.containers.output.OutputFrame.OutputType.STDERR;
+import static org.testcontainers.containers.output.OutputFrame.OutputType.STDOUT;
 
 
 @Mojo(name = "h2spec", defaultPhase = LifecyclePhase.INTEGRATION_TEST,
@@ -61,37 +73,37 @@ public class Http2SpecMojo extends AbstractMojo
     /**
      * The port on which the Server will listen.
      */
-    @Parameter(defaultValue = "-1", property="port", required = true)
+    @Parameter(property="h2spec.port", defaultValue = "-1", required = true)
     private int port;
 
     /**
      * Timeout in seconds
      */
-    @Parameter(defaultValue = "2", property="timeout")
+    @Parameter(property="h2spec.timeout", defaultValue = "2")
     private int timeout;
 
     /**
      * Maximum length of HTTP headers
      */
-    @Parameter(defaultValue = "4000", property="maxHeaderLength")
+    @Parameter(property="h2spec.maxHeaderLength", defaultValue = "4000")
     private int maxHeaderLength;
 
     /**
      * A list of cases to exclude during the test. Default is to exclude none.
      */
-    @Parameter(property = "excludeSpecs")
+    @Parameter(property = "h2spec.excludeSpecs")
     private List<String> excludeSpecs;
 
     /**
      * The class which is used to startup the Server. It will pass the port in as argument to the main(...) method.
      */
-    @Parameter(property = "mainClass", required = true)
+    @Parameter(property = "h2spec.mainClass", required = true)
     private String mainClass;
 
     /**
      * The number of milliseconds to max wait for the server to startup. Default is 10000 ms
      */
-    @Parameter(property = "waitTime")
+    @Parameter(property = "h2spec.waitTime")
     private long waitTime = 10000;
 
     /**
@@ -111,15 +123,12 @@ public class Http2SpecMojo extends AbstractMojo
     private boolean verbose;
 
     @Parameter(property = "h2spec.version", defaultValue = DEFAULT_VERSION )
-    private String h2specVersion = DEFAULT_VERSION;
+    private String h2specVersion;
 
-    @Parameter(property = "h2spec.useBinaries", defaultValue = "false" )
-    /**
-     * using h2spec binaries or docker image default is to use docker image
-     */
-    private boolean useBinaries;
+    @Parameter(property = "h2spec.containerName", defaultValue = "summerwind/h2spec")
+    private String h2specContainerName;
 
-    @Component
+    @Parameter(defaultValue = "${project}", readonly = true)
     private MavenProject project;
 
     @SuppressWarnings("unchecked")
@@ -176,6 +185,7 @@ public class Http2SpecMojo extends AbstractMojo
                 // Get some random free port
                 port = findRandomOpenPortOnAllLocalInterfaces();
             }
+            ClassLoader ori = Thread.currentThread().getContextClassLoader();
             runner = new Thread(() ->
             {
                 try
@@ -188,13 +198,16 @@ public class Http2SpecMojo extends AbstractMojo
                 catch (Exception e)
                 {
                     error.set(e);
+                } finally
+                {
+                    Thread.currentThread().setContextClassLoader(ori);
                 }
             });
             runner.setDaemon(true);
             runner.start();
             try
             {
-                // wait for 50 milliseconds to give the server some time to startup
+                // wait for 500 milliseconds to give the server some time to startup
                 Thread.sleep(500);
             }
             catch (InterruptedException ignore)
@@ -265,54 +278,60 @@ public class Http2SpecMojo extends AbstractMojo
                 }
 
                 File outputDirectory = new File(project.getBuild().getDirectory());
-                H2SpecTestSuite.Config config = new H2SpecTestSuite.Config();
-                config.log = getLog();
-                config.outputDirectory = outputDirectory;
-                config.port = port;
-                config.timeout = timeout;
-                config.excludeSpecs = new HashSet<>(excludeSpecs);
-                config.junitFileName = this.junitFileName;
-                config.verbose = verbose;
-                config.version = h2specVersion;
+
                 List<Failure> allFailures;
                 List<Failure> nonIgnoredFailures = new ArrayList<>();
                 List<Failure> ignoredFailures = new ArrayList<>();
 
-                if (useBinaries)
+                File reportsDirectory = new File(outputDirectory, "surefire-reports");
+                if (!Files.exists(reportsDirectory.toPath()))
                 {
-                    allFailures = H2SpecTestSuite.runH2Spec( config );
-                }
-                else
-                {
-                    File reportsDirectory = new File(config.outputDirectory, "surefire-reports");
-                    if (!Files.exists( reportsDirectory.toPath()))
+                    getLog().debug("Reports directory " + reportsDirectory.getAbsolutePath() + " does not exist, try creating it...");
+                    if (reportsDirectory.mkdirs())
                     {
-                        config.log.debug("Reports directory " + reportsDirectory.getAbsolutePath() + " does not exist, try creating it...");
-                        if (reportsDirectory.mkdirs())
-                        {
-                            config.log.debug("Reports directory " + reportsDirectory.getAbsolutePath() + " created.");
-                        }
-                        else
-                        {
-                            config.log.debug("Failed to create report directory");
-                        }
+                        getLog().debug("Reports directory " + reportsDirectory.getAbsolutePath() + " created.");
                     }
-
-                    File junitFile = new File(reportsDirectory, config.junitFileName);
-                    GenericContainer h2spec =
-                        new GenericContainer( DockerImageName.parse("summerwind/h2spec:" + h2specVersion))
-                            .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(getClass().getName())));
-                    h2spec.setWaitStrategy( new LogMessageWaitStrategy().withRegEx(".*"));
-                    h2spec.setPortBindings(Arrays.asList(Integer.toString(port)));
-                    String command = String.format("-p %d -j %s -o %d --max-header-length %d",
-                                                   config.port, junitFile.getAbsolutePath(),
-                                                   config.timeout, config.maxHeaderLength);
-                    h2spec.setCommand( command );
-                    h2spec.start();
-                    allFailures = H2SpecTestSuite.parseReports(config.log, junitFile.getParentFile(),
-                                                                config.excludeSpecs);
-
+                    else
+                    {
+                        getLog().debug("Failed to create report directory");
+                    }
                 }
+
+                File junitFile = new File(reportsDirectory, junitFileName);
+                String imageName = h2specContainerName + ":" + h2specVersion;
+                String command = String.format( "-h %s -p %d -j %s -o %d --max-header-length %d",
+                                                "host.testcontainers.internal",
+                                                port,
+                                                junitFile.getAbsolutePath(),
+                                                timeout,
+                                                maxHeaderLength );
+                if ( verbose )
+                {
+                    command = command + " -v";
+                }
+
+                getLog().info( "running image: " + imageName + " with command: " + command);
+
+                Testcontainers.exposeHostPorts(port);
+                //PathUtils
+                try (GenericContainer h2spec = new GenericContainer( DockerImageName.parse( imageName ) )
+                            .withFileSystemBind(outputDirectory.getAbsolutePath(),
+                                                outputDirectory.getAbsolutePath(), BindMode.READ_WRITE))
+                {
+                    if(verbose)
+                    {
+                        h2spec.withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger(getClass().getName())));
+                    }
+                    h2spec.setWaitStrategy(new LogMessageWaitStrategy().withStartLine( "Finished in " ) );
+                    h2spec.setPortBindings( Arrays.asList( Integer.toString( port ) ) );
+
+
+                    h2spec.withCommand( command );
+                    h2spec.start();
+                    allFailures =
+                        H2SpecTestSuite.parseReports( getLog(), junitFile.getParentFile(), new HashSet<>(excludeSpecs) );
+                }
+
 
                 allFailures.forEach(failure ->
                 {
@@ -340,7 +359,7 @@ public class Http2SpecMojo extends AbstractMojo
                     getLog().info("All test cases passed. " + ignoredFailures.size() + " test cases ignored.");
                 }
             }
-            catch (IOException e)
+            catch (Exception e)
             {
                 throw new MojoExecutionException(e.getMessage(), e);
             }
@@ -380,6 +399,59 @@ public class Http2SpecMojo extends AbstractMojo
                     System.err.println("Can't close server socket.");
                 }
             }
+        }
+    }
+
+    static class LogMessageWaitStrategy extends AbstractWaitStrategy
+    {
+
+        private String startLine;
+
+        private int times = 1;
+
+        @Override
+        protected void waitUntilReady() {
+            WaitingConsumer waitingConsumer = new WaitingConsumer();
+
+            LogContainerCmd cmd = DockerClientFactory.instance().client().logContainerCmd( waitStrategyTarget.getContainerId())
+                .withFollowStream(true)
+                .withSince(0)
+                .withStdOut(true)
+                .withStdErr(true);
+
+            try
+            {
+                try (FrameConsumerResultCallback callback = new FrameConsumerResultCallback()) {
+                    callback.addConsumer(STDOUT, waitingConsumer);
+                    callback.addConsumer(STDERR, waitingConsumer);
+
+                    cmd.exec(callback);
+
+                    Predicate<OutputFrame> waitPredicate = outputFrame -> {
+                        String line = outputFrame.getUtf8String();
+                        return line.startsWith( startLine );
+                    };
+                    try {
+                        waitingConsumer.waitUntil( waitPredicate, startupTimeout.getSeconds(), TimeUnit.SECONDS, times);
+                    } catch ( TimeoutException e) {
+                        throw new ContainerLaunchException( "Timed out waiting for log output matching '" + startLine + "'");
+                    }
+                }
+            }
+            catch ( IOException e )
+            {
+                e.printStackTrace();
+            }
+        }
+
+        public LogMessageWaitStrategy withStartLine( String startLine) {
+            this.startLine = startLine;
+            return this;
+        }
+
+        public LogMessageWaitStrategy withStartLine( int times) {
+            this.times = times;
+            return this;
         }
     }
 }
